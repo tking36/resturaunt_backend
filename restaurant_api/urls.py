124bacc3d0e0a8434da89e04135ce9899274a490
Andrew <Asaganda@gmail.com>
2024-05-11 12:21:08 -0400

    login functions and the client receives jwt tokens - yay

--- restaurant_api/urls.py
+++ restaurant_api/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('home/', views.HomeView.as_view(), name='home'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('login/', views.LoginView.as_view(), name='login'),
 ]
--- restaurant_api/views.py
+++ restaurant_api/views.py
@@ -12,7 +12,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import AllowAny
 from django.contrib.auth.forms import UserCreationForm
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
 import json
+from django.contrib.auth import authenticate
 
 class RestaurantList(generics.ListCreateAPIView):
     queryset = Restaurant.objects.all().order_by('id') # tell django how to retrieve all objects from the DB, order by id ascending
@@ -35,6 +37,30 @@ class HomeView(APIView):
     def get(self, request):
         content = {'message': 'Welcome to the JWT Auth page using React and Django'}
         return Response(content)
+
+class LoginView(APIView):
+    def post(self, request, format=None):
+        data = json.loads(request.body.decode('utf-8'))
+        print(f'request data, {data}')
+        # Extract username and password from request data
+        username = data.get('username')
+        password = data.get('password')
+
+        # Authenticate user
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+            # Generate access token and refresh token
+            refresh = RefreshToken.for_user(user)
+
+            # Return tokens in response
+            return Response({
+                'access_token': str(refresh.access_token),
+                'refresh_token': str(refresh)
+            }, status=status.HTTP_200_OK)
+        else:
+            # Authentication failed
+            return Response({ 'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
 class LogoutView(APIView):
     permission_classes = (IsAuthenticated, )
